c893919e79ef3308743ef83d23142a93b95ee9e3
Priyam Biswas <priyambiswaslg@gmail.com>
2023-03-13 15:21:20 +0530

    Bus code added

---
+++ Bus.java
@@ -0,0 +1,156 @@
+class Bus{ 
+	public static void main(String args[])
+		{
+
+	
+System.out.println("Bus travels from Tenzing Norgay Bus stand to Shantipara Bus stand");
+System.out.println("Starting point is Tenzing Norgay bus stand");
+System.out.println("1st stoppage is Howrah petrol pump bus stand");
+System.out.println("2nd stoppage is Jhankarmore bus stand");
+System.out.println("3rd stoppage is Jalpaimore bus stand");
+System.out.println("4th stoppage is Noukaghat bus stand");
+System.out.println("5th stoppage is Teenbatti more bus stand");
+System.out.println("6th stoppage is Fulbari bus stand");
+System.out.println("7th stoppage is Fatapukur bus stand");
+System.out.println("8th stoppage is Raninagar bus stand");
+System.out.println("9th stoppage is  Mohitnagar bus stand");
+System.out.println("10th stoppage is 73 more bus stand");
+System.out.println("11th stoppage is Jhabari more bus stand");
+System.out.println("12th stoppage is Haldibari more bus stand");
+System.out.println("Ending stoppage is Shantipara bus stand");
+
+
+
+	System.out.println("Fair from Tenzing Norgay Bus stand to Howrah petrol pump bus stand is 10");
+	System.out.println("Fair from Tenzing Norgay Bus stand to Jhankar more bus stand is 10");
+	System.out.println("Fair from Tenzing Norgay Bus stand to Jalpai more  bus stand is 10");
+	System.out.println("Fair from Tenzing Norgay Bus stand to Noukaghat bus stand is 10");
+	System.out.println("Fair from Tenzing Norgay Bus stand to Teenbatti more bus stand is 15");
+	System.out.println("Fair from Tenzing Norgay Bus stand to  Fulbari bus stand is 20");
+	System.out.println("Fair from Tenzing Norgay Bus stand to  Fatapukur bus stand is 25");
+	System.out.println("Fair from Tenzing Norgay Bus stand to Raninagar bus stand is 30");
+	System.out.println("Fair from Tenzing Norgay Bus stand to Mohitnagar bus stand is 32");
+	System.out.println("Fair from Tenzing Norgay Bus stand to 73 more bus stand is 34");
+	System.out.println("Fair from Tenzing Norgay Bus stand to Jhabari more bus stand is 35");
+	System.out.println("Fair from Tenzing Norgay Bus stand to Haldibari more bus stand is 38");
+	System.out.println("Fair from Tenzing Norgay Bus stand to  Shantipara bus stand is 38");
+	
+	System.out.println("Fair from Howrah petrol punp Bus stand to Jhankakar more bus stand is 10");
+	System.out.println("Fair from Howrah petrol punp Bus stand to Jalpaimore bus stand is 10");
+	System.out.println("Fair from Howrah petrol punp Bus stand to Noukaghat bus stand is 10");
+	System.out.println("Fair from Howrah petrol punp Bus stand to Teenbatti more bus stand is 10");
+	System.out.println("Fair from Howrah petrol punp Bus stand to Fulbari bus stand is 20");
+	System.out.println("Fair from Howrah petrol punp Bus stand to Fatapukur bus stand is 25");
+	System.out.println("Fair from Howrah petrol punp Bus stand to Raninagar bus stand is 30");
+	System.out.println("Fair from Howrah petrol punp Bus stand to Mohitnagar bus stand is 32");
+	System.out.println("Fair from Howrah petrol punp Bus stand to 73 more bus stand is 34");
+	System.out.println("Fair from Howrah petrol punp Bus stand to Jhabari more bus stand is 35");
+	System.out.println("Fair from Howrah petrol punp Bus stand to Haldibari more bus stand is 38");
+	System.out.println("Fair from Howrah petrol punp Bus stand to Shantipara  bus stand is 38");
+	
+	
+	System.out.println("Fair from Jhankar more Bus stand to Jalpaimore bus stand is 10");
+	System.out.println("Fair from Jhankar more Bus stand to Noukaghat bus stand is 10");
+	System.out.println("Fair from Jhankar more Bus stand to Teenbatti more bus stand is 10");
+	System.out.println("Fair from Jhankar more Bus stand to Fulbari bus stand is 20");
+	System.out.println("Fair from Jhankar more Bus stand to Fatapukur bus stand is 25");
+	System.out.println("Fair from Jhankar more Bus stand to Raninagar bus stand is 30");
+	System.out.println("Fair from Jhankar more Bus stand to Mohitnagar bus stand is 32");
+	System.out.println("Fair from Jhankar more Bus stand to 73 more bus stand is 34");
+	System.out.println("Fair from Jhankar more Bus stand to Jhabari more bus stand is 35");
+	System.out.println("Fair from Jhankar more Bus stand to Haldibari more bus stand is 38");
+	System.out.println("Fair from Jhankar more Bus stand to Shantipara bus stand is 38");
+	
+
+	System.out.println("Fair from Jalpai more Bus stand to Noukaghat bus stand is 10");
+	System.out.println("Fair from Jalpai more Bus stand to Teenbatti more bus stand is 10");
+	System.out.println("Fair from Jalpai more Bus stand to Fulbari bus stand is 20");
+	System.out.println("Fair from Jalpai more Bus stand to Fatapukur bus stand is 25");
+	System.out.println("Fair from Jalpai more Bus stand to Raninagar bus stand is 30");
+	System.out.println("Fair from Jalpai more Bus stand to Mohitnagar bus stand is 32");
+	System.out.println("Fair from Jalpai more Bus stand to 73 more bus stand is 34");
+	System.out.println("Fair from Jalpai more Bus stand to Jhabarimore bus stand is 35");
+	System.out.println("Fair from Jalpai more Bus stand to Haldibarimore bus stand is 38");
+	System.out.println("Fair from Jalpai more Bus stand to Shantipara bus stand is 38");
+	
+	
+	System.out.println("Fair from Noukaghat Bus stand to Teenbatti more bus stand is 10");
+	System.out.println("Fair from Noukaghat Bus stand to Fulbari bus stand is 20");
+	System.out.println("Fair from Noukaghat Bus stand to Fatapukur  bus stand is 25");
+	System.out.println("Fair from Noukaghat Bus stand to Raninagar bus stand is 30");
+	System.out.println("Fair from Noukaghat Bus stand to Mohitnagar bus stand is 32");
+	System.out.println("Fair from Noukaghat Bus stand to 73 more bus stand is 34");
+	System.out.println("Fair from Noukaghat Bus stand to Jhabari more bus stand is 35");
+	System.out.println("Fair from Noukaghat Bus stand to Haldibari more bus stand is 38");
+	System.out.println("Fair from Noukaghat Bus stand to Shantipara bus stand is 38");
+	
+
+	System.out.println("Fair from Teenbatti more Bus stand to Fulbari bus stand is 20");
+	System.out.println("Fair from Teenbatti more Bus stand to Fatapukur  bus stand is 25");
+	System.out.println("Fair from Teenvatti more Bus stand to Raninagar bus stand is 30");
+	System.out.println("Fair from Teenbatti more Bus stand to Mohitnagar bus stand is 32");
+	System.out.println("Fair from Teenbatti more Bus stand to 73 more bus stand is 34");
+	System.out.println("Fair from Teenbatti more Bus stand to Jhabari more bus stand is 35");
+	System.out.println("Fair from Teenbatti more Bus stand to Haldibari more bus stand is 38");
+	System.out.println("Fair from Teenbatti more Bus stand to Shantipara bus stand is 38");
+
+
+	System.out.println("Fair from Fulbari Bus stand to Fatapukur  bus stand is 15");
+	System.out.println("Fair from Fulbari Bus stand to Raninagar bus stand is 22");
+	System.out.println("Fair from Fulbari Bus stand to Mohitnagar bus stand is 25");
+	System.out.println("Fair from Fulbari  Bus stand to 73 more bus stand is 28");
+	System.out.println("Fair from Fulbari  Bus stand to Jhabari more bus stand is 30");
+	System.out.println("Fair from Fulbari  Bus stand to Haldibari more bus stand is 32");
+	System.out.println("Fair from Fulbari Bus stand to Shantipara bus stand is 35");
+
+
+	System.out.println("Fair from Fatapukur Bus stand to Raninagar bus stand is 15");
+	System.out.println("Fair from Fatapukur Bus stand to Mohitnagar bus stand is 18");
+	System.out.println("Fair from Fatapukur Bus stand to 73 more bus stand is 20");
+	System.out.println("Fair from Fatapukur Bus stand to Jhabari more bus stand is 22");
+	System.out.println("Fair from Fuatapukur Bus stand to Haldibari more bus stand is 25");
+	System.out.println("Fair from Fatapukur Bus stand to Shantipara bus stand is 28");
+
+
+	System.out.println("Fair from Raninagar Bus stand to Mohitnagar bus stand is 8");
+	System.out.println("Fair from Raninagar Bus stand to 73 more bus stand is 10");
+	System.out.println("Fair from Raninagar Bus stand to Jhabari more bus stand is 10");
+	System.out.println("Fair from Raninagar Bus stand to Haldibari more bus stand is 12");
+	System.out.println("Fair from Raninagar Bus stand to Shantipara bus stand is 15");
+
+
+	System.out.println("Fair from Mohitnagar Bus stand to 73 more bus stand is 10");
+	System.out.println("Fair from Mohitnagar Bus stand to Jhabari more bus stand is 10");
+	System.out.println("Fair from Mohitnagar Bus stand to Haldibari more bus stand is 12");
+	System.out.println("Fair from Mohitnagar Bus stand to Shantipara bus stand is 15");
+
+
+	System.out.println("Fair from 73 more Bus stand to Jhabari more bus stand is 10");
+	System.out.println("Fair from 73 more Bus stand to Haldibari more bus stand is 10");
+	System.out.println("Fair from 73 more Bus stand to Shantipara bus stand is 10");
+
+
+	System.out.println("Fair from Jhabari more Bus stand to Haldibari more bus stand is 10");
+	System.out.println("Fair from Jhabari more Bus stand to Shantipara bus stand is 10");
+
+
+	System.out.println("Fair from Haldibari more Bus stand to Shantipara bus stand is 10");
+
+		}
+}	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
